10fa21950cb3d0920c7eecfa5af2fd468fa0c1bf
Wellington Ideao <wellington.ideao@gmail.com>
2021-07-02 14:32:13 -0300

    User autentication and login using fbv

--- bookmarks/settings/base.py
+++ bookmarks/settings/base.py
@@ -18,6 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # Application definition
 
 INSTALLED_APPS = [
+    'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
